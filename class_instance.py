ffb5538fbab768944e37b6a5b6ee9522099166e1
Yuna <rdking9812@gmail.com>
2022-04-05 16:21:14 +0900

    Add magic methods to class_instance.py

--- class_instance.py
+++ class_instance.py
@@ -1,22 +1,34 @@
+
 class User:
-    def say_hello(some_user):
-        print("안녕하세요, 저는 {}입니다".format(some_user.name))
+    count = 0
+    def __init__(self,name,email,password):
+        self.name = name
+        self.email = email
+        self.password = password 
+        User.count += 1
+
+    def __str__(self):
+        #던더 메소드 (더블 언더 메소드)
+        return "사용자:{}, 이메일:{}".format(self.name, self.email)
+
+    def say_hello(self):
+        print("안녕하세요, 저는 {}입니다".format(self.name))
+
+    def login(self,email,my_password):
+        if (self.email == email and self.password == my_password):
+            print("로그인 성공")
+        else:
+            print("로그인 실패")
+
+    def check_name(self,name):
+        return self.name == name
 
 
-user1 = User()
-user2 = User()
-user3 = User()
 
-user1.name = "김대위"
-user1.email = "captain@codeit.kr"
-user1.password = "12345"
+user4 = User('Yeonsoo','yeonsoo@gmail.com','12345')
+user5 = User('Yuna','rdking9812@naver.com','45678')
 
-user2.name = "강영훈"
-user2.email = "younghoon@codeit.kr"
-user2.password = '98765'
 
-user3.name = "최지웅"
-user3.email = "jiwoong@codeit.kr"
-user3.password = '78945'
 
-User.say_hello(user1)
+print(user4)
+print(User.count)
